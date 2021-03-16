a9d2726eb6217d2ef5ba954628cd260f443e5317
Sebastian Kopf <sebkopf>
2021-03-15 22:58:06 -0600

    revamp formatting with significant digits and add y axis break plotting

--- scripts/plotting_functions.R
+++ scripts/plotting_functions.R
@@ -42,28 +42,47 @@ theme_figure <- function(legend = TRUE, grid = TRUE, plot_margin = c(1, 1, 1, 1)
 #' 
 #' This is useful for including numbers on plots as text elements.
 #' 
-#' @param signif number of significant digits
-#' @param sci_notation cutoff when to start using scientific notation
-#' @param sci_as_latex scientific notiation as latex
-#' @param include_plus whether to include plus sign
+#' @param signif number of significant digits to display
+#' @param no_sci_min use scientific notation for abs(x)<=no_sci_min, only used if always_sci = FALSE
+#' @param no_sci_max use scientific notation for abs(x)>=no_sci_max, only used if always_sci = FALSE
+#' @param alawys_sci set to TRUE to always use scientific notation
+#' @param sci_as_latex whether to print scientific notation as latex
+#' @param include_plus whether to include plus sign for positive numbers (x>0)
 #' @return textual representation of the number matching the parameter settings
-format_with_signif <- function(x, signif = 2, sci_notation = 1e-5, sci_as_latex = FALSE, include_plus = FALSE) {
+format_with_signif <- function(x, signif = 2, no_sci_min = 1e-5, no_sci_max = 1e5, always_sci = FALSE, sci_as_latex = FALSE, include_plus = FALSE) {
   if(signif <= 0) stop("this function only supports numbers with at least 1 significant digit")
   x <- base::signif(x, digits = signif)
   n_decimals = ifelse(x == 0, 0, log10(abs(x)))
-  n_decimals = ifelse(
-    n_decimals < 0,
-    -floor(n_decimals) + signif - 1L,
-    -ceiling(n_decimals) + signif
-  )
+  # find decimals depending on whether it's an exact power of 10 or not
+  exact <- (n_decimals %% 1) < .Machine$double.eps^0.5
+  n_decimals <- ifelse(exact, n_decimals, floor(n_decimals))
+  # use same decimals for true 0 as the smallest abs(x)
+  zeros <- abs(x) < .Machine$double.eps^0.5
+  if (any(zeros)) {
+    min_decimals <- min(n_decimals[!zeros], na.rm = TRUE)
+    n_decimals <- ifelse(zeros, min_decimals, n_decimals)
+  }
+  # determine formatting
   plus <- if(include_plus) "+" else ""
-  pow <- if(sci_as_latex) "\\\\cdot{}10^{-%.0f}" else "e-%.0f"
+  pow <- if(sci_as_latex) "\\\\cdot{}10^{%.0f}" else "e%.0f"
   ifelse(
-    abs(x) > sci_notation,
-    sprintf(sprintf("%%%s.%0.ff", plus, n_decimals), x),
+    !always_sci & abs(x) > no_sci_min & abs(x) < no_sci_max,
+    # no scientific notation
+    sprintf(sprintf("%%%s.%0.ff", plus, -n_decimals), x),
+    # scientific notation
     sprintf(sprintf("%%%s.%0.ff%s", plus, signif - 1L, pow), 
-            x * 10^(n_decimals - signif + 1L), (n_decimals - signif + 1L))
+            x * 10^(-n_decimals), n_decimals)
   )
+}
+
+#' Parse scientific notation formatter
+#' @example 
+#' p + scale_y_continuous(labels = parsed_sci_format(signif = 3))
+parsed_sci_format <- function(signif = 2, include_plus = FALSE) {
+  require("latex2exp")
+  function(x) {
+    latex2exp::TeX(format_with_signif(x, signif = signif, always_sci = TRUE, sci_as_latex = TRUE, include_plus = include_plus))
+  }
 }
 
 #' Latex labeller
@@ -106,3 +125,105 @@ latex_labeller <- function(labels, ...) {
   else return(labels$..x..)
 }
 class(latex_labeller) <- c("function", "labeller")
+
+#' @param pb_* bottom subplot parameters
+#' @param pt_* top subplot parameters
+plot_with_y_gap <- function(
+  pb, pb_ylim = NULL, pb_breaks = waiver(), pb_labels = labels, pb_gap_marker_offset = 0.02, pb_gap_marker_width = 0.5, pb_relative_size = 1,
+  pt = pb, pt_ylim = NULL, pt_breaks = waiver(), pt_labels = labels, pt_gap_marker_offset = 0, pt_gap_marker_width = 0.5, pt_relative_size = 1,
+  ylab = pb$labels$y, xlab = pb$labels$x, labels = waiver()
+) {
+  force(xlab)
+  pb$labels$x <- NULL
+  pt <- pt + coord_cartesian(ylim = pt_ylim) + 
+    scale_y_continuous(NULL, expand = c(0, 0), breaks = pt_breaks, labels = pt_labels, sec.axis = sec_axis(~ .)) +
+    theme(
+      panel.border = element_blank(),
+      plot.margin = margin(b = -3, t = 3, r = 3),
+      axis.line.x.top = element_line(color = 'black'),
+      axis.line.y.left = element_y_axis_with_gap(bottom_marker = pt_gap_marker_offset, width = pt_gap_marker_width),
+      axis.line.y.right = element_line(color = 'black'),
+      axis.ticks.y.right = element_blank(),
+      axis.text.y.right = element_blank(),
+      axis.title.x = element_blank(),
+      axis.text.x = element_blank(),
+      axis.ticks.x = element_blank()
+    )
+  # add sec axis for x on top plot
+  position_scales <- map_lgl(pt$scales$scales, is, "ScaleContinuousPosition")
+  x_aesthetics <- map_lgl(pt$scales$scales, ~any(.x$aesthetics == "x"))
+  x_idx <- which(position_scales & x_aesthetics)
+  if (length(x_idx) > 0) 
+    pt$scales$scales[[x_idx[1]]]$secondary.axis <- sec_axis(~ .)
+  else
+    pt <- pt + scale_x_continuous(sec.axis = sec_axis(~ .))
+  
+  pb <- pb + coord_cartesian(ylim = pb_ylim) + 
+    scale_y_continuous(NULL, expand = c(0, 0), pb_breaks, labels = pb_labels, sec.axis = sec_axis(~ .)) +
+    theme(
+      panel.border = element_blank(),
+      plot.margin = margin(t = -3, b = 3, r = 3),
+      axis.line.x.bottom = element_line(color = 'black'),
+      axis.line.y.left = element_y_axis_with_gap(top_marker = pb_gap_marker_offset, width = pb_gap_marker_width),
+      axis.line.y.right = element_line(color = 'black'),
+      axis.ticks.y.right = element_blank(),
+      axis.text.y.right = element_blank(),
+      axis.title.x.top = element_blank(),
+      axis.text.x.top = element_blank(),
+      axis.ticks.x.top = element_blank()
+    )
+  
+  # combined plots
+  p <- cowplot::plot_grid(
+    pt, pb, ncol = 1, align = "v", rel_heights = c(pt_relative_size, pb_relative_size)
+  )
+  
+  # add axis labels
+  y_label_grob <- NULL
+  x_label_grob <- NULL
+  if (!is.null(ylab)) {
+    y_label_grob <- grid::textGrob(ylab, gp = element_render(pb$theme, "axis.text.y")$children[[1]]$gp, rot=90)
+  } 
+  if (!is.null(xlab)) {
+    x_label_grob <- grid::textGrob(xlab, gp = element_render(pb$theme, "axis.text.x")$children[[1]]$gp)
+  }
+  gridExtra::grid.arrange(gridExtra::arrangeGrob(p, left = y_label_grob, bottom = x_label_grob))
+}
+
+
+#' function to use y axis with gap in theme
+#' @param bottom_marker offset of the gap marker at the bottom of the scale (if NULL, no bottom marker is drawn)
+#' @param top_marker offset of the gap marker at the top of the scale (if NULL, no top marker is drawn)
+#' @param width of markers (relative)
+element_y_axis_with_gap <- function(bottom_marker = NULL, top_marker = NULL, width = 0.5) {
+  structure(
+    list(bottom_marker = bottom_marker, top_marker = top_marker, width = width), 
+    class = c("element_y_axis_with_gap","element_blank", "element") 
+  )
+}
+
+# actual axis structure (don't call this directly)
+element_grob.element_y_axis_with_gap <- function(element, ...)  {
+  width <- if (!is.null(element$width)) element$width else 0.5
+  if (!is.null(element$bottom_marker)) {
+    bottom_offset <- element$bottom_marker
+    bottom_grob <- grid::segmentsGrob(x0 = 1 - width, y0 = bottom_offset, x1 = 1 + width, y1 = bottom_offset)
+  } else {
+    bottom_offset <- 0
+    bottom_grob <- NULL
+  }
+  if (!is.null(element$top_marker)) {
+    top_offset <- element$top_marker
+    top_grob <- grid::segmentsGrob(x0 = 1 - width, y0 = 1 - top_offset, x1 = 1 + width, y1 = 1 - top_offset)
+  } else {
+    top_offset <- 0
+    top_grob <- NULL
+  }
+  
+  grid::gList(
+    grid::segmentsGrob(x0 = 1, y0 = bottom_offset, x1 = 1, y1 = 1 - top_offset),
+    bottom_grob,
+    top_grob
+  )
+}
+
